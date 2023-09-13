c76a66aa93341e58e83e87df65feee2b52efb07b
Monoget <monoget2@gmail.com>
2023-09-13 13:46:53 +0600

    update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 new WOW().init();
+
+const darkModeSwitch = document.getElementById("darkModeSwitch");
+const body = document.body;
+
+darkModeSwitch.addEventListener("change", () => {
+    if (darkModeSwitch.checked) {
+        body.classList.add("dark-mode");
+    } else {
+        body.classList.remove("dark-mode");
+    }
+});
