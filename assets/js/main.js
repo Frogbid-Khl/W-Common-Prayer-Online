09265c35e743815379975fe36b1315368a50e916
Monoget <monoget2@gmail.com>
2023-09-13 13:58:24 +0600

    update site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,42 @@
 new WOW().init();
 
+// Function to set a cookie with a given name, value, and expiration date
+function setCookie(name, value, days) {
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + days);
+    const cookieValue = encodeURIComponent(value) + ((days === null) ? '' : `; expires=${expirationDate.toUTCString()}`);
+    document.cookie = `${name}=${cookieValue}; path=/`;
+}
+
+// Function to get the value of a cookie by name
+function getCookie(name) {
+    const cookies = document.cookie.split('; ');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].split('=');
+        if (cookie[0] === name) {
+            return decodeURIComponent(cookie[1]);
+        }
+    }
+    return null;
+}
+
 const darkModeSwitch = document.getElementById("darkModeSwitch");
 const body = document.body;
+
+// Check the user's dark mode preference from the cookie
+const darkModePreference = getCookie("darkModePreference");
+
+if (darkModePreference === "dark") {
+    darkModeSwitch.checked = true;
+    body.classList.add("dark-mode");
+}
 
 darkModeSwitch.addEventListener("change", () => {
     if (darkModeSwitch.checked) {
         body.classList.add("dark-mode");
+        setCookie("darkModePreference", "dark", 365); // Set the dark mode preference in a cookie for 1 year
     } else {
         body.classList.remove("dark-mode");
+        setCookie("darkModePreference", "light", 365); // Set the light mode preference in a cookie for 1 year
     }
 });
