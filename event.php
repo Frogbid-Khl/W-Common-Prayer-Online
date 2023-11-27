d19e314d273a4dcc05da7b046a2428870e10dc57
Monoget <monoget2@gmail.com>
2023-11-27 11:40:14 +0600

    update url

--- event.php
+++ event.php
@@ -27,7 +27,7 @@ for ($j = 0; $j < $row; $j++) {
 
 if ($row == 0) {
     echo "<script>
-                window.location.href='../psalter';
+                window.location.href='../404';
                 </script>";
 }
 ?>
