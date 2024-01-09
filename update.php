e8e76dc632829f5790040ae0ec32340cafcf50a4
Monoget Saha <monoget2@gmail.com>
2024-01-09 23:53:02 +0600

    update page develop

---
+++ update.php
@@ -0,0 +1,100 @@
+<?php
+date_default_timezone_set("America/New_York");
+require_once("include/dbController.php");
+$db_handle = new DBController();
+
+$currentMonthkalendar = date('Y-m');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link href="assets/images/favicon.ico" rel="icon" type="image/x-icon">
+
+    <!-- Developer: Monoget Saha (www.monoget.com.bd) -->
+    <!-- Company: FrogBid (www.frogbid.com) -->
+
+    <title>BETA PREVIEW - Common Prayer Online</title>
+    <link href="assets/vendor/Bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="assets/vendor/FontAwesome/css/all.min.css" rel="stylesheet"/>
+    <link href='assets/vendor/Animate/animate.min.css' rel='stylesheet'/>
+    <link href="assets/css/style.css" rel="stylesheet"/>
+
+    <style>
+        .cpo-home-btn {
+            height: 105px;
+        }
+    </style>
+</head>
+<body>
+<section class="fixed-top cpo-bg">
+    <div class="container-fluid form-check form-switch mt-2 position-absolute" style="padding-left: unset">
+        <div class="row mx-lg-0 mx-auto" style="max-width: 125px">
+            <div class="col-4">
+                <label class="form-check-label" for="darkModeSwitch">Light</label>
+            </div>
+            <div class="col-4">
+                <input class="form-check-input" type="checkbox" role="switch" id="darkModeSwitch"
+                       style="margin-left: unset;">
+            </div>
+            <div class="col-4">
+                <label class="form-check-label" for="darkModeSwitch">Dark</label>
+            </div>
+        </div>
+    </div>
+
+    <!-- Mobile Menu Button -->
+    <div id="mobile-menu-button" class="d-lg-none pt-5">
+        <div class="cpo-dynamic-bg">
+            <div class="container-fluid pt-3 pb-2">
+                <div class="row">
+                    <div class="col-6">
+                        <h5>
+                            Menu
+                        </h5>
+                    </div>
+                    <div class="col-6 text-end">
+                        <i class="fa-solid fa-bars"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="container cpo-body-padding-top">
+    <div class="row">
+        <div class="col-12 pt-5 text-center">
+            <h1 class="cpo-content-page-title">
+                BETA PREVIEW
+            </h1>
+        </div>
+        <div class="col-12 text-center mt-4 mb-4">
+            <img src="assets/images/pray.webp" alt="" class="img-fluid" style="max-height: 350px"/>
+            <h5 class="mt-3">
+                IMPROVE YOUR DAILY PRAYER LIFE WITH COMMONPRAYERONLINE.COM
+            </h5>
+            <img src="assets/images/pattern.png" alt="" class="img-fluid"/>
+            <p>
+                We're delighted that you're enjoying Common PrayerOnline.com and finding it valuable for your daily
+                prayer routine. Our goal is to make this the most user-friendly Daily Office site, centered on the 1928
+                BCP. Although you may notice some of the daily readings and liturgical colors showing up incorrectly,
+                rest assured that we are working hard to resolve these issues as soon as possible. We appreciate your
+                patience as we make final tweaks and adjustments.
+            </p>
+        </div>
+    </div>
+</section>
+
+
+<button onclick="topFunction()" id="cpo-scroll-to-top" title="Go to top"><i class="fa-solid fa-arrow-up"></i></button>
+
+<script src="assets/vendor/Bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/jQuery/jquery-3.6.4.min.js"></script>
+<script src="assets/vendor/OwlCarousel/js/owl.carousel.min.js"></script>
+<script src="assets/vendor/Wow/wow.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
